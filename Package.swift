53ee00372e1f2183ef56b6c2628575ba5b3ba997
adiman <adiman.muhammad@mimos.my>
2024-11-20 09:44:41 +0800

    init

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MiSignetHelper",
+    platforms: [
+        .iOS(.v15),
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -20,5 +23,6 @@ let package = Package(
             name: "MiSignetHelperTests",
             dependencies: ["MiSignetHelper"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v4]
 )
