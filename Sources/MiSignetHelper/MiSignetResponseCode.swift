aaa9a1d9cdc24a4382e01cd6df70568e817ee926
mydigitalid-dev <mydigitalid.dev@gmail.com>
2024-12-06 10:05:30 +0800

    Update MiSignetResponseCode.swift
    .

--- Sources/MiSignetHelper/MiSignetResponseCode.swift
+++ Sources/MiSignetHelper/MiSignetResponseCode.swift
@@ -11,7 +11,7 @@ import Foundation
 public enum MiSignetResponseCode: Int {
     case ok = 1
     case invalidDataFormatError = 401
-    case invalidRequestInformationError = 402
+    case invalidRequestInformationError = 402 //FIXME: intent error in doc?
     case unauthorizedApplicationError = 403
     case permissionDeniedByUserError = 404
     case rscNotMatchError = 406
@@ -21,13 +21,15 @@ public enum MiSignetResponseCode: Int {
     case signatureGenerationError = 410
     case trustedCertificateChainNotFoundError = 411
     case invalidCertificateError = 412
-    case signatureExpiredError = 413
+    case signatureExpiredError = 413 //FIXME: missing in doc
     case invalidSignatureError = 414
-    case certificateExpiredError = 415
+    case certificateExpiredError = 415 //FIXME: missing in doc
     case invalidCertificateChainError = 416
-    case invalidJWSFormatError = 417
+    case invalidJWSFormatError = 417 //FIXME: missing in doc
     case sensorDataError = 418
+    //FIXME: 419 missing in doc
     case jwsSignatureGenerationError = 420
+    //FIXME: 421 missing in doc
     case publicKeyNotMatchError = 422
     case appNotInKeyGeneratedStateError = 424
     case appNotInRegisteredStateError = 425
@@ -35,5 +37,5 @@ public enum MiSignetResponseCode: Int {
     case encryptionError = 427
     case nameNotMatchError = 428
     case appAlreadyRegisteredError = 429
-    case signatureVerifiedWithoutTrustedCertificateChainError = 430
+    case signatureVerifiedWithoutTrustedCertificateChainError = 430//FIXME: missing in doc
 }
